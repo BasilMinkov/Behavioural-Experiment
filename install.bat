08436d2157c9709a7f387b98f71d35a31271a9ec
Basil Minkov <proveyourselfmail@gmail.com>
2019-03-05 15:10:12 +0300

    Delete install.bat

--- install.bat
+++
@@ -1,5 +0,0 @@
-cd %~dp0
-python-3.6.3.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-python -m pip install expyriment future pygame pyopengl
-echo cd %~dp0 > run.bat
-echo python run_experiment.py >> run.bat
